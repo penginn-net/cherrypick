b86dcbb7ac7895ce197426c2ad75122e28400b95
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-12-25 18:01:16 +0900

    renote

--- packages/backend/test-federation/test/fetch-outbox.test.ts
+++ packages/backend/test-federation/test/fetch-outbox.test.ts
@@ -5,6 +5,7 @@ import { createAccount, type LoginUser, resolveRemoteUser } from './utils.js';
 describe('fetch-outbox', () => {
 	let alice: LoginUser, bob: LoginUser;
 	let bobInA: Misskey.entities.UserDetailedNotMe, aliceInB: Misskey.entities.UserDetailedNotMe;
+	let note: Misskey.entities.Note, renote: Misskey.entities.Note;
 
 	beforeAll(async () => {
 		[alice, bob] = await Promise.all([
@@ -16,11 +17,22 @@ describe('fetch-outbox', () => {
 			resolveRemoteUser('b.test', bob.id, alice),
 			resolveRemoteUser('a.test', alice.id, bob),
 		]);
+		note = (await alice.client.request('notes/create', { text: 'I am Yojo!' })).createdNote;
+		renote = (await alice.client.request('notes/create', { renoteId: note.id })).createdNote;
 	});
 	test('includeAnnounce true', async () => {
-		const note = (await alice.client.request('notes/create', { text: 'I am Yojo!' })).createdNote;
 		await bob.client.request('ap/fetch-outbox', { userId: aliceInB.id, wait: true, includeAnnounce: true });
 		const fetch_notes = await bob.client.request('users/notes', { userId: aliceInB.id, withReplies: false, withRenotes: true });
-		strictEqual(fetch_notes.map(note => note.text)[0], note.text);
+		strictEqual(fetch_notes.length, 2);
+		strictEqual(fetch_notes.map(note => note.text), [null, note.text]);
+		strictEqual(fetch_notes.map(note => note.createdAt), [renote.createdAt, note.createdAt]);
+		strictEqual(fetch_notes[0].renote?.id, fetch_notes[1].id);
+	});
+	test('includeAnnounce false', async () => {
+		await bob.client.request('ap/fetch-outbox', { userId: aliceInB.id, wait: true, includeAnnounce: false });
+		const fetch_notes = await bob.client.request('users/notes', { userId: aliceInB.id, withReplies: false, withRenotes: true });
+		strictEqual(fetch_notes.length, 1);
+		strictEqual(fetch_notes.map(note => note.text), [note.text]);
+		strictEqual(fetch_notes.map(note => note.createdAt), [note.createdAt]);
 	});
 });
