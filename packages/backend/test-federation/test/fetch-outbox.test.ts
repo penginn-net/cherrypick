22a2a14757274a2c52867df43f1f8d7f747c97d1
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-12-25 19:51:25 +0900

    いろいろ

--- packages/backend/test-federation/test/fetch-outbox.test.ts
+++ packages/backend/test-federation/test/fetch-outbox.test.ts
@@ -3,36 +3,48 @@ import * as Misskey from 'cherrypick-js';
 import { createAccount, type LoginUser, resolveRemoteUser } from './utils.js';
 
 describe('fetch-outbox', () => {
-	let alice: LoginUser, bob: LoginUser;
-	let bobInA: Misskey.entities.UserDetailedNotMe, aliceInB: Misskey.entities.UserDetailedNotMe;
-	let note: Misskey.entities.Note, renote: Misskey.entities.Note;
+	let alice: LoginUser, bob: LoginUser, carol:LoginUser;
+	let bobInAliceHost: Misskey.entities.UserDetailedNotMe, carolInAliceHost: Misskey.entities.UserDetailedNotMe;
+	let bobNote: Misskey.entities.Note, bobRenote: Misskey.entities.Note;
+	let carolNote: Misskey.entities.Note, carolRenote: Misskey.entities.Note;
 
 	beforeAll(async () => {
-		[alice, bob] = await Promise.all([
+		[alice, bob, carol] = await Promise.all([
 			createAccount('a.test'),
+			createAccount('b.test'),
 			createAccount('b.test'),
 		]);
 
-		[bobInA, aliceInB] = await Promise.all([
+		[bobInAliceHost, carolInAliceHost] = await Promise.all([
 			resolveRemoteUser('b.test', bob.id, alice),
-			resolveRemoteUser('a.test', alice.id, bob),
+			resolveRemoteUser('b.test', carol.id, alice),
 		]);
-		note = (await alice.client.request('notes/create', { text: 'I am Yojo!' })).createdNote;
-		renote = (await alice.client.request('notes/create', { renoteId: note.id })).createdNote;
+		bobNote = (await bob.client.request('notes/create', { text: 'I am Bob!' })).createdNote;
+		bobRenote = (await bob.client.request('notes/create', { renoteId: bobNote.id })).createdNote;
+		carolNote = (await carol.client.request('notes/create', { text: 'I am Carol!' })).createdNote;
+		carolRenote = (await bob.client.request('notes/create', { renoteId: carolNote.id })).createdNote;
 	});
 	test('includeAnnounce false', async () => {
-		await bob.client.request('ap/fetch-outbox', { userId: aliceInB.id, wait: true, includeAnnounce: false });
-		const fetch_notes = await bob.client.request('users/notes', { userId: aliceInB.id, withReplies: false, withRenotes: true });
+		await alice.client.request('ap/fetch-outbox', { userId: bobInAliceHost.id, wait: true, includeAnnounce: false });
+		const fetch_notes = await alice.client.request('users/notes', { userId: bobInAliceHost.id, withReplies: false, withRenotes: true });
 		strictEqual(fetch_notes.length, 1);
-		deepStrictEqual(fetch_notes.map(note => note.text), [note.text]);
-		deepStrictEqual(fetch_notes.map(note => note.createdAt), [note.createdAt]);
+		deepStrictEqual(fetch_notes.map(note => note.text?.length), [bobNote.text?.length]);
+		deepStrictEqual(fetch_notes.map(note => note.createdAt), [bobNote.createdAt]);
 	});
-	test('includeAnnounce true', async () => {
-		await bob.client.request('ap/fetch-outbox', { userId: aliceInB.id, wait: true, includeAnnounce: true });
-		const fetch_notes = await bob.client.request('users/notes', { userId: aliceInB.id, withReplies: false, withRenotes: true });
+	test('includeAnnounce true(Know User)', async () => {
+		await alice.client.request('ap/fetch-outbox', { userId: bobInAliceHost.id, wait: true, includeAnnounce: true });
+		const fetch_notes = await alice.client.request('users/notes', { userId: bobInAliceHost.id, withReplies: false, withRenotes: true });
 		strictEqual(fetch_notes.length, 2);
-		deepStrictEqual(fetch_notes.map(note => note.text), [null, note.text]);
-		deepStrictEqual(fetch_notes.map(note => note.createdAt), [renote.createdAt, note.createdAt]);
+		deepStrictEqual(fetch_notes.map(note => note.text?.length), [null, bobNote.text?.length]);
+		deepStrictEqual(fetch_notes.map(note => note.createdAt), [bobRenote.createdAt, bobNote.createdAt]);
+		strictEqual(fetch_notes[0].renote?.id, fetch_notes[1].id);
+	});
+	test('includeAnnounce true(New User)', async () => {
+		await alice.client.request('ap/fetch-outbox', { userId: carolInAliceHost.id, wait: true, includeAnnounce: true });
+		const fetch_notes = await alice.client.request('users/notes', { userId: carolInAliceHost.id, withReplies: false, withRenotes: true });
+		strictEqual(fetch_notes.length, 2);
+		deepStrictEqual(fetch_notes.map(note => note.text?.length), [null, carolNote.text?.length]);
+		deepStrictEqual(fetch_notes.map(note => note.createdAt), [carolRenote.createdAt, carolNote.createdAt]);
 		strictEqual(fetch_notes[0].renote?.id, fetch_notes[1].id);
 	});
 });
